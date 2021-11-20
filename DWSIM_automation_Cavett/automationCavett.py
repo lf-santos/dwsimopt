43a5e5e9348c9aa78f49cb7218d496056d5652c8
lf-santos <lfs.francisco.95@gmail.com>
2021-11-20 14:34:26 -0300

    automationCavett.py working properly

--- DWSIM_automation_Cavett/automationCavett.py
+++ DWSIM_automation_Cavett/automationCavett.py
@@ -1,3 +1,4 @@
+## C#-related libraries
 import pythoncom
 pythoncom.CoInitialize()
 
@@ -22,6 +23,7 @@ clr.AddReference(dwsimpath + "DWSIM.MathOps.dll")
 clr.AddReference(dwsimpath + "TcpComm.dll")
 clr.AddReference(dwsimpath + "Microsoft.ServiceBus.dll")
 
+# importing DWSIM classes from clr references
 from DWSIM.Interfaces.Enums.GraphicObjects import ObjectType
 from DWSIM.Thermodynamics import Streams, PropertyPackages
 from DWSIM.UnitOperations import UnitOperations
@@ -32,14 +34,43 @@ from os import getcwd
 pathLocal = getcwd()
 Directory.SetCurrentDirectory(dwsimpath)
 
+# math related importing
+import numpy as np
+import time
+
 # create automation manager
-
 interf = Automation2()
-
-sim = interf.LoadFlowsheet(pathLocal + "\DWSIM_automation_MATLAB\CavettProblem.dwxml")
+sim = interf.LoadFlowsheet(pathLocal + "\DWSIM_automation_Cavett\CavettProblem.dwxml")
 
 # use CAPE-OPEN interfaces to manipulate objects
 feed = sim.GetFlowsheetSimulationObject("2")
 vap_out = sim.GetFlowsheetSimulationObject("8")
 liq_out = sim.GetFlowsheetSimulationObject("18")
 
+# mass flow rate values in kg/s
+flows = [170.0, 180.0, 190.0, 200.0] 
+flowData = np.zeros(4)
+
+for i in range(len(flows)):
+    # setting values to simulation
+    feed.SetMassFlow(flows[i])
+    # calculate the flowsheet (run the simulation)
+    print("Running simulation with F = ", flows[i], " kg/s, please wait...")
+    interf.CalculateFlowsheet2(sim)
+    # time.sleep(1)
+    t0 = time.time()
+    while (not sim.Solved and time.time()-t0<1):
+        print("Simulation status: ", sim.Solved)
+        time.sleep(0.1)
+    # check for errors during the last run
+    if not sim.Solved:
+        print("Error solving flowsheet: ", sim.ErrorMessage)
+    # get vapor outlet mass flow value
+    vflow = vap_out.GetMassFlow()
+    # get liquid outlet mass flow value
+    lflow = liq_out.GetMassFlow()
+    # display results
+    print("Simulation run # ",i," results:\nFeed: ", flows[i], ", Vapor: ",vflow,", Liquid: ",lflow," kg/s\nMass balance error: ", (flows[i] - vflow - lflow)," kg/s\n")
+
+# Saving the file
+# interf.SaveFlowsheet2(sim, sim.FilePath)
